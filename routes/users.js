9fb64f8401d8f33dfe5df703e8965412e53d761b
Syndicate555 <saziz028@uottawa.ca>
2020-05-22 14:00:02 -0400

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,8 @@ const router = express.Router();
 // @route POST api/users
 // @desc Register a user
 // @access Public
-router.post();
+router.post("/", (req, res) => {
+  res.send("Register a user");
+});
+
+module.exports = router;
