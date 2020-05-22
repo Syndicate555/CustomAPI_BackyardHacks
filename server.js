cbde98ee9bfc5c1d7753c9a72ab0832d33dd8ed8
Syndicate555 <saziz028@uottawa.ca>
2020-05-22 12:49:41 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,3 +5,6 @@ const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => res.json({ msg: "Welcome to the API...." }));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// Define Routes
+app.use("/api/users", require("./routes/users"));
