2e2bfab4c83a752810ddbe64c645ce3248c2f91b
Syndicate555 <saziz028@uottawa.ca>
2020-05-22 12:50:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 // Define Routes
 app.use("/api/users", require("./routes/users"));
-app.use("/api/users", require("./routes/users"));
-app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/food", require("./routes/food"));
