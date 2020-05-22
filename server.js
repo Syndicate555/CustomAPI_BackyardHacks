6c0a80ab77d7939d76898213e3be111a94a86e2f
Syndicate555 <saziz028@uottawa.ca>
2020-05-22 12:52:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => res.json({ msg: "Welcome to the API...." }));
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 // Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/food", require("./routes/food"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
