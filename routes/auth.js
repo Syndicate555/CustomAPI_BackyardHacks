d19db71483494a7d115b9609eff8d0a687f0e6ae
Syndicate555 <saziz028@uottawa.ca>
2020-05-22 14:03:43 -0400

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -8,4 +8,13 @@ router.post("/", (req, res) => {
   res.send("Register a user");
 });
 
+// @route POST api/users
+// @desc Register a user
+// @access Public
+router.post("/", (req, res) => {
+  res.send("Register a user");
+});
+
+module.exports = router;
+
 module.exports = router;
