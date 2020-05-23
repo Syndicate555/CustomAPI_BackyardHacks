c84e9a6a2af6e6195da24511b75783900614e488
Syndicate555 <saziz028@uottawa.ca>
2020-05-23 00:49:15 -0400

    changes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => res.json({ msg: "Welcome to the API...." }));
 // Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
-app.use("/api/food", require("./routes/food"));
+app.use("/api/foods", require("./routes/foods"));
 
 const PORT = process.env.PORT || 5000;
 
